9ee3233589e1ffa06267e20a9552b4192e37d9b1
pnyani <me@nyani.kr>
2026-05-29 15:00:44 +0900

    feat: add source files in src/

---
+++ src/aes_internal.h
@@ -0,0 +1,8 @@
+#ifndef AES_INTERNAL_H
+#define AES_INTERNAL_H
+
+/* Shared internals between aes.c and aes_variant.c. Not for external use. */
+
+#include <stdint.h>
+
+#endif /* AES_INTERNAL_H */
---
+++ src/aes_variant.c
@@ -0,0 +1,2 @@
+#include "aes_internal.h"
+#include "aes_variant.h"
